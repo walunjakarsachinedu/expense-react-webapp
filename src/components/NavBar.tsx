bd0a887d134133757dbd34cd0430caee15eb1734
Sachin Walunjakar <walunjakarsachin@gmail.com>
2024-09-11 21:19:51 +0530

    improve navigation bar and heading icon styling

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -9,11 +9,9 @@ export default function NavBar() {
         className='blur-bg flex justify-content-center'
         style={{
           width: "100%", 
-          border: "solid 1px var(--surface-border)", 
           padding: 10,
           paddingRight: 15, 
-          boxShadow: "0 4px 8px rgba(0, 0, 0, 0.2)",
-          background: "rgba(255,255,255,0.05)"
+          background: "rgba(200,200,200,.02)"
         }}
       > 
         <Row>
