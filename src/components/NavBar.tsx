e585c24776466ab2280c1542c031c85d0ecdc6dc
Sachin Walunjakar <walunjakarsachin@gmail.com>
2024-12-19 06:17:28 +0530

    prevented text wrapping in navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,7 +5,7 @@ import "./NavBar.css";
 
 export default function NavBar() {
   return (
-    <div className="nav-bar">
+    <div className="nav-bar white-space-nowrap">
       <div
         className="blur-bg flex justify-content-center"
         style={{
