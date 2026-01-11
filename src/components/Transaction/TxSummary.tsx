d37fb6304405d566f9b26be031a9cd36fc30ecf5
Sachin Walunjakar <walunjakarsachin@gmail.com>
2026-01-11 11:58:56 +0530

    made TxSummary to wrap large content

--- src/components/Transaction/TxSummary.tsx
+++ src/components/Transaction/TxSummary.tsx
@@ -19,18 +19,22 @@ const TxSummary = () => {
           header="Summary"
           style={{ backgroundColor: "var(--surface-ground) !important" }}
         >
-          <div className="flex justify-content-center gap-3 md:gap-5 p-4">
+          <div className="flex justify-content-center flex-wrap row-gap-4 gap-3 md:gap-5 p-4">
             <LabeledNumber label="Income" number={incomeTotal}></LabeledNumber>
-            <div className="text-2xl text-500">-</div>
-            <LabeledNumber
-              label="Expense"
-              number={expenseTotal}
-            ></LabeledNumber>
-            <div className="text-2xl text-500">=</div>
-            <LabeledNumber
-              label="Remaining"
-              number={incomeTotal - expenseTotal}
-            ></LabeledNumber>
+            <div className="flex justify-content-center gap-3">
+              <div className="text-2xl text-500">-</div>
+              <LabeledNumber
+                label="Expense"
+                number={expenseTotal}
+              ></LabeledNumber>
+            </div>
+            <div className="flex justify-content-center gap-3">
+              <div className="text-2xl text-500">=</div>
+              <LabeledNumber
+                label="Remaining"
+                number={incomeTotal - expenseTotal}
+              ></LabeledNumber>
+            </div>
           </div>
         </AccordionTab>
       </Accordion>
